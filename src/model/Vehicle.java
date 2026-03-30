d067b17eee2c3e44340a2132a3d57792679dc53f
minhquang25q09-arch <minhquang25q09@gmail.com>
2026-03-30 21:47:23 +0700

    Create Vehicle.java

---
+++ src/model/Vehicle.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Vehicle extends Item {
+
+    private String brand;
+    private int mileage;
+
+    public Vehicle(String name, String description, double startPrice,
+                   Seller seller,
+                   double buyNowPrice, double reservePrice,
+                   String condition, int quantity,
+                   String brand, int mileage) {
+
+        super(name, description, startPrice, seller,
+              buyNowPrice, reservePrice,
+              condition, quantity);
+
+        this.brand = brand;
+        this.mileage = mileage;
+    }
+
+    public String getBrand() { return brand; }
+    public int getMileage() { return mileage; }
+
+    @Override
+    public String getItemType() {
+        return "VEHICLE";
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Brand: " + brand);
+        System.out.println("Mileage: " + mileage);
+    }
+}
