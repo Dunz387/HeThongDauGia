6439c04bc88fb0355189e4065caabe85f3fcb6d6
minhquang25q09-arch <minhquang25q09@gmail.com>
2026-03-30 21:41:56 +0700

    Create Art.java

---
+++ src/model/Art.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Art extends Item {
+
+    private String artist;
+    private int year;
+
+    public Art(String name, String description, double startPrice,
+               Seller seller,
+               double buyNowPrice, double reservePrice,
+               String condition, int quantity,
+               String artist, int year) {
+
+        super(name, description, startPrice, seller,
+              buyNowPrice, reservePrice,
+              condition, quantity);
+
+        this.artist = artist;
+        this.year = year;
+    }
+
+    public String getArtist() { return artist; }
+    public int getYear() { return year; }
+
+    @Override
+    public String getItemType() {
+        return "ART";
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Artist: " + artist);
+        System.out.println("Year: " + year);
+    }
+}
