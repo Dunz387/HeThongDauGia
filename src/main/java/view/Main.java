a12561da2e0a9a36fba841aca8c1033a522679e3
Dunz387 <buiquangdungdn@gmail.com>
2026-04-27 22:06:03 +0700

    Update Main để chạy

---
+++ src/main/java/view/Main.java
@@ -0,0 +1,14 @@
+package view;
+
+// import view.AuthenticationUI.LoginView.Login;
+import view.AuthenticationUI.RegisterView.Register;
+
+public class Main {
+    public static void main(String[] args) {
+        // Run Login:
+        // Login.main(args); 
+
+        // Run Register:
+        Register.main(args); 
+    }
+}
