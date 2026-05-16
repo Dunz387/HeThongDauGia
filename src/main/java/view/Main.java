c0698526be18f7ccf97063afdc9316979548ac6d
Dunz387 <buiquangdungdn@gmail.com>
2026-05-16 21:30:02 +0700

    Update Main.java

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -9,16 +9,18 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         // --- PHẦN THÊM MỚI: XỬ LÝ NGOẠI LỆ TOÀN CỤC ---
         view.utility.GlobalExceptionHandler.setupHandler();
 
         // --- PHẦN THÊM MỚI: KẾT NỐI MẠNG ---
         LOGGER.info("Đang kết nối đến Server...");
-        boolean isConnected = ClientNetworkManager.getInstance().connect("0.tcp.ap.ngrok.io", 12456);
+        boolean isConnected = ClientNetworkManager.getInstance().connect("localhost", 8080);
 
         if (!isConnected) {
-            LOGGER.severe("❌ LỖI: Không tìm thấy Server. Vui lòng chạy AuctionServer.java trước rồi khởi động lại App!");
+            LOGGER.severe(
+                    "❌ LỖI: Không tìm thấy Server. Vui lòng chạy AuctionServer.java trước rồi khởi động lại App!");
             return; // Dừng khởi chạy giao diện nếu không có mạng
         }
         // ------------------------------------
