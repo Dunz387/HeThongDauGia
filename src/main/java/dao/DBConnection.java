19295165b98d05bdee177f5ef28240652d3f88d6
Dunz387 <buiquangdungdn@gmail.com>
2026-05-18 18:47:41 +0700

    matrix OS

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -1,11 +1,15 @@
 package dao;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:sqlite:auction.db";
+    // Tái cấu trúc mã nguồn sử dụng API java.nio.file.Path để tương thích trên nhiều OS
+    private static final Path DB_PATH = Paths.get(System.getProperty("user.dir"), "auction.db");
+    private static final String URL = "jdbc:sqlite:" + DB_PATH.toString();
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
